b0db894ae704b042efc6988eefe018b97de2c129
unknown <d146cordeliert@21-UC36-16.ad.campus-eni.fr>
2015-10-22 10:42:57 +0200

    * Use space not tab
    Signed-off-by: unknown <d146cordeliert@21-UC36-16.ad.campus-eni.fr>

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -16,26 +16,35 @@ class ThemeController extends Controller
     public function __construct()
     {
     }
+        
+    //----------------------------
+    //API ------------------------
+    //----------------------------
     
      /**
-     * getAll() return all Theme in database
-     * @return jsonArray return a json array with all accounts
+     * @api {get} /themes Request Themes information
+     * @apiName getAll
+     * @apiGroup Themes
+     *
+     * @apiSuccess {Boolean} error an error occur
+     * @apiSuccess {String} message description of action
+     * @apiSuccess {Array} data all themes
      */
     public function getAll(Request $request)
     {
-        $themes = Theme::orderBy('name');
         try
         {
+            $themes = Theme::orderBy('name');
             if($request->has('page'))
             {
                  $page = $request->input('page');
                  $results = $themes->paginate(8);
-				 
-				 if($results->lastPage() < $page)
-				 {
-					$request->replace(array('page' => $results->lastPage())); //change the page
-					$results = $themes->paginate(8);
-			     }				 
+                 
+                 if($results->lastPage() < $page)
+                 {
+                    $request->replace(array('page' => $results->lastPage())); //change the page
+                    $results = $themes->paginate(8);
+                 }                 
 
                  return response()->json(["error"=> false, "message" =>"", "data" => $results->toArray()]);
             }
@@ -50,68 +59,105 @@ class ThemeController extends Controller
         }
     }
     
+     /**
+     * @api {delete} /themes/{id} delete a specific theme
+     * @apiName delete
+     * @apiGroup Themes
+     *
+     * @apiParam {Number} id Theme unique ID
+     *
+     * @apiSuccess {Boolean} error an error occur
+     * @apiSuccess {String} message description of action
+     * @apiSuccess {Array} data current deleted theme
+     */
     public function deleteDelete($id)
     {
-        $theme = Theme::find($id);
-        
-        if(!$theme)
-            return response()->json(["error" => true, "message" => Lang::get('theme.notFound'), "data" => []]);
-        else
+        try
         {
-            $theme->delete();
-            return response()->json(["error" => true, "message" => Lang::get('theme.delete', ["name" => $theme->name]), "data" => $theme]);
+            $theme = Theme::find($id);
+            
+            if(!$theme)
+                return response()->json(["error" => true, "message" => Lang::get('theme.notFound'), "data" => []]);
+            else
+            {
+                $theme->delete();
+                return response()->json(["error" => true, "message" => Lang::get('theme.delete', ["name" => $theme->name]), "data" => $theme]);
+            }
+        }
+        catch(\Exception $e)
+        {
+            return response()->json(["error" => true, "message" => $e->getMessage(), "data" => []]); //fail something is wrong
         }
     }
     
+    /**
+     * @api {post} /themes/ Add theme in database
+     * @apiName Add
+     * @apiGroup Themes
+     *
+     * @apiParam {String} name Theme name
+     * @apiParam {String} description Theme description
+     *
+     * @apiSuccess {Boolean} error an error occur
+     * @apiSuccess {String} message description of action
+     * @apiSuccess {Array} data current new theme
+     */
     public function postAdd(Request $request)
     {
-        //rules to apply of each field
-        $rules = array(
-            'name'             => 'required|min:2|max:25',
-            'description'      => 'required|min:10|max:100',
-        );
-        
-        //try to validate
-        $validator = Validator::make($request->all(), $rules);
-        if ($validator->fails()) 
+        try
         {
-            $errors = $validator->messages()->getMessages();
-            $errorsJson = array();
-            $fields = array("name", "description");
+            //rules to apply of each field
+            $rules = array(
+                'name'             => 'required|min:2|max:25',
+                'description'      => 'required|min:10|max:100',
+            );
             
-            //----
-            //Name
-            //----
-            if(array_key_exists("name", $errors))
-                for($i = 0; $i < count($errors["name"]); $i++)
-                {
-                    $key = $errors["name"][$i];
-                    $value = (strpos($key, '.min.') !== false) ? 2 : 25; 
-                    array_push($errorsJson,  Lang::get('validator.'.$key, ["name" => "Nom", "value" => $value]));
-                }
+            //try to validate
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) 
+            {
+                $errors = $validator->messages()->getMessages();
+                $errorsJson = array();
+                $fields = array("name", "description");
                 
-            //-----------
-            //Description
-            //-----------
-            if(array_key_exists("description", $errors))
-                for($i = 0; $i < count($errors["description"]); $i++)
-                {
-                   $key = $errors["description"][$i];
-                   $value = (strpos($key, '.min.') !== false) ? 10 : 100; 
-                   array_push($errorsJson,  Lang::get('validator.'.$errors["description"][$i], ["name" => "Déscription", "value" => $value]));
-                }
+                //----
+                //Name
+                //----
+                if(array_key_exists("name", $errors))
+                    for($i = 0; $i < count($errors["name"]); $i++)
+                    {
+                        $key = $errors["name"][$i];
+                        $value = (strpos($key, '.min.') !== false) ? 2 : 25; 
+                        array_push($errorsJson,  Lang::get('validator.'.$key, ["name" => "Nom", "value" => $value]));
+                    }
+                    
+                //-----------
+                //Description
+                //-----------
+                if(array_key_exists("description", $errors))
+                    for($i = 0; $i < count($errors["description"]); $i++)
+                    {
+                       $key = $errors["description"][$i];
+                       $value = (strpos($key, '.min.') !== false) ? 10 : 100; 
+                       array_push($errorsJson,  Lang::get('validator.'.$errors["description"][$i], ["name" => "Déscription", "value" => $value]));
+                    }
+                    
+                //error
+                return response()->json(["error" => true, "message" => $errorsJson, "data" => []]);
+            }
+            else //ok
+            {
+                $theme = new Theme();
+                $theme->name = $request->input('name');
+                $theme->description = $request->input('description');
+                $theme->save();
                 
-            //error
-            return response()->json(["error" => true, "message" => $errorsJson, "data" => []]);
+                return response()->json(["error" => false, "message" => Lang::get('theme.add', ["name" => $theme->name]), "data" => $theme]);
+            }
         }
-        else //ok
+        catch(\Exception $e)
         {
-            $theme = new Theme();
-            $theme->name = $request->input('name');
-            $theme->description = $request->input('description');
-            $theme->save();
-            
-            return response()->json(["error" => false, "message" => Lang::get('theme.add', ["name" => $theme->name]), "data" => $theme]);
+            return response()->json(["error" => true, "message" => $e->getMessage(), "data" => []]); //fail something is wrong
         }
     }
 }
